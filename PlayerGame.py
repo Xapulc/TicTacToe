72448da05a05153b25d82f5634f120e3418c27b6
Xapulc <xarik1999@gmail.com>
2019-01-05 12:06:16 +0300

    add probability second move, add probability play some games

--- PlayerGame.py
+++ PlayerGame.py
@@ -20,28 +20,48 @@ class PlayerGame(object):
         self.win = 1
         self.draw = 0
         self.lose = -1
+        self.comp_turn = 0
 
     def start(self):
         """
         Start a game with computer
         """
         print("Welcome to TicTacToe, v. 2.7218281828459045")
-        input("Press Enter to start c:")
-
         while True:
-            self.player_move()
-            if self.game.winner() == 1:
-                print("You win!!!")
-                print("Congratulations!!!")
-                break
-            elif len(self.game) == 9:
-                print("Nobody win")
+            while True:
+                try:
+                    who_first = input("Who if first: 0 - player, 1 - computer?")
+                    assert who_first in ('0', '1')
+                except AssertionError:
+                    print("Please, write 0 or 1")
+                    continue
+
                 break
 
-            self.comp_move()
-            if self.game.winner() == 0:
-                print("You lose")
-                print("Congratulations!!!")
+            self.comp_turn = int(who_first)
+            while True:
+                self.player_move() if self.comp_turn == 0 else self.comp_move()
+                if self.game.winner() == 1:
+                    print("You win!!!") if self.comp_turn == 0 else print("You lose")
+                    print("Congratulations!!!")
+                    break
+                elif len(self.game) == 9:
+                    print("Nobody win")
+                    break
+
+                self.comp_move() if self.comp_turn == 0 else self.player_move()
+                if self.game.winner() == 0:
+                    print("You lose") if self.comp_turn == 0 else print("You win!!!")
+                    print("Congratulations!!!")
+                    break
+
+            agreement = input("Do you want to play new game: 1 - yes, other - no")
+            if agreement == '1':
+                print("Preparing...")
+                self.game = TicTacToe()
+                self.data = load_dict_from_file()
+                continue
+            else:
                 break
 
     def player_move(self):
@@ -67,10 +87,12 @@ class PlayerGame(object):
         """
         A computer move
         """
-        keys = set(self.data.keys())
-        for key in keys:
-            if key[:len(self.game)] != tuple(self.game):
-                self.data.pop(key)
+        if len(self.game) > 0:
+            keys = set(self.data.keys())
+            for key in keys:
+                if key[:len(self.game)] != tuple(self.game):
+                    self.data.pop(key)
+
         if self.data:
             self.game.add(self.__do_professional_move())
         else:
@@ -86,9 +108,9 @@ class PlayerGame(object):
         count_matrix = [[[0, 0] for _ in range(3)] for _ in range(3)]
         for game, res in self.data.items():
             move = game[len(self.game)]
-            if res == 0:  # win "x"
+            if res == self.comp_turn:  # win computer
                 count_matrix[move.x][move.y][0] += self.win/(len(game)-len(self.game))**4
-            elif res == 1:  # win "o"
+            elif res == 1 - self.comp_turn:  # win player
                 count_matrix[move.x][move.y][0] += self.lose/(len(game)-len(self.game))**4
             else:
                 count_matrix[move.x][move.y][0] += self.draw/(len(game)-len(self.game))**4
