eb1dc2296b90dcd8c47649bda6496a838eae9d2d
Xapulc <xarik1999@gmail.com>
2019-01-07 22:26:10 +0300

    Revert "Merge branch 'master' of C:\Users\Виктор\PycharmProjects\TicTacToe with conflicts."
    This reverts commit 815ee1d9

--- LearningTeacher.py
+++ LearningTeacher.py
@@ -1,5 +1,5 @@
-import GenerateDataForGame as gd
 import FileWorker as fw
+from LearningGame import LearningGame
 from TicTacToe import TicTacToe
 
 
@@ -7,21 +7,17 @@ def create_learning_teacher_game():
     """
     :return: moves in the game and result (who winner)
     """
-    game = TicTacToe()
-    end = False
-    while not end:
-        end = game.move(gd.generate_random_elem(game))
-
+    game, _ = LearningGame({}, {}).start(1)
     return game, game.winner()
 
 
 if __name__ == "__main__":
-    count = 100000
+    count = 15000
     dic = {}
-
-    for _ in range(count):
+    for i in range(count):
         game, res = create_learning_teacher_game()
         dic[tuple(game)] = res
-
+        if i+1 % (count // 100) == 0:
+            print(f"complete on {100 * ((i+1)/count):.0f}%")
     print(len(dic.keys()))
     fw.save_dict_to_file(dic, "games.txt")
